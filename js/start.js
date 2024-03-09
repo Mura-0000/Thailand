56cf276f7bbba5f9e0fc42cb5b1f719b080ac143
Andreil <guff-0000@mail.ru>
2024-03-09 12:41:57 +0300

    helloy commit

---
+++ js/start.js
@@ -0,0 +1,12 @@
+
+// Menu
+// Menu
+
+function openModal() {
+   document.getElementById("modal").style.top = "-20px";
+}
+
+function closeModal() {
+   document.getElementById("modal").style.top = "-460px";
+}
+
